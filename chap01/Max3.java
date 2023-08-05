d51d77d4891c4460b49b7e63071fac1cbeacab1a
Wooya <wooyya03@naver.com>
2023-08-05 21:30:59 +0900

    Median

--- chap01/Max3.java
+++ chap01/Max3.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
         Scanner stdIn = new Scanner(System.in);
 
         System.out.println("세 정수의 최댓값을 구합니다. ");
-        System.out.println("a의 값: "); int a = stdIn.nextInt();
-        System.out.println("b의 값: "); int b = stdIn.nextInt();
-        System.out.println("c의 값: "); int c = stdIn.nextInt();
+        System.out.print("a의 값: "); int a = stdIn.nextInt();
+        System.out.print("b의 값: "); int b = stdIn.nextInt();
+        System.out.print("c의 값: "); int c = stdIn.nextInt();
 
         int max = a;
         if (b > max) max = b;

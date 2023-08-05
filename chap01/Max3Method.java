35f93a557b8a7811db3802112affc87c63d81e4d
Wooya <wooyya03@naver.com>
2023-08-05 21:48:48 +0900

    Update Max3Method.java

--- chap01/Max3Method.java
+++ chap01/Max3Method.java
@@ -15,7 +15,9 @@ public class Max3Method {
         System.out.println("max3(3,2,2)="+max3(3,2,2));
         System.out.println("max3(3,1,2)="+max3(3,1,2));
         System.out.println("max3(3,2,3)="+max3(3,2,3));
+        System.out.println("max3(2,1,3)="+max3(2,1,3));
         System.out.println("max3(3,3,2)="+max3(3,3,2)); 
+        System.out.println("max3(3,3,3)="+max3(3,3,3));
         System.out.println("max3(2,2,3)="+max3(2,2,3));
         System.out.println("max3(2,3,1)="+max3(2,3,1));
         System.out.println("max3(2,3,2)="+max3(2,3,2));

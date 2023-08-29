1b3f79d07bf375401a258f49cd675b06b5b12c1e
Wooya <wooyya03@naver.com>
2023-08-29 18:13:23 +0900

    Q10

--- chap01/Q10.java
+++ chap01/Q10.java
@@ -8,5 +8,6 @@ public class Q10 {
          System.out.print("자릿수 입력: ");
          String A = sc.nextLine();
          System.out.printf("그 수는 %s자리입니다.", A.length());
+         sc.close();
     }
 }

608ae273ca670a3c3b7b096f59c2832b262a8d53
Wooya <wooyya03@naver.com>
2023-08-29 13:06:33 +0900

    실습 1-9

--- chap01/SumFor2.java
+++ chap01/SumFor2.java
@@ -10,18 +10,14 @@ public class SumFor2 {
         System.out.println("1부터 n까지의 합을 구합니다.");
 
         do {
-            System.out.println("n값: ");
+            System.out.print("n값: ");
             n = stdIn.nextInt();
         } while (n <= 0);
         int sum = 0;
 
         for (int i = 1; i <= n; i++){
-            if (i < n)
-                System.out.println(i + "+ ");
-            else
-                System.out.println(i + "= ");
-            sum += i;
+           sum += i;
     }
-    System.out.println(sum);
+    System.out.println("1부터 " + n + "까지의 합은" + sum + "입니다.");
     }
 }

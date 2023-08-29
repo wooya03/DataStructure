ffd3eb69899f609478d689eb9f84caf3e4ff992a
Wooya <wooyya03@naver.com>
2023-08-29 18:27:40 +0900

    Q14

--- chap01/Q14.java
+++ chap01/Q14.java
@@ -3,12 +3,41 @@ package chap01;
 import java.util.Scanner;
 
 public class Q14 {
-  static void traiangleLB(int n){
-    for(int i = n; i <= 1; i--){
-        for(int j = 1; j <= i; j++){
-            System.out.print("*");
-        }
-        System.out.println();
+  static void triangleLB(int n){
+    for (int i = 1; i <= n ; i++){
+      for (int j = 1; j <= i; j++)
+          System.out.print("*");
+      System.out.println();
+    }
+  }
+  static void triangleLU(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = n; j >= i; j--){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+  static void triangleRU(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = 1; j <= i; j++){
+        System.out.print(" ");
+      }
+      for(int j = n; j >= i; j--){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+  static void triangleRB(int n){
+    for(int i = 1; i <= n; i++){
+      for(int j = n; j >= i; j--){
+        System.out.print(" ");
+      }
+      for(int j = 1; j <= i; j++){
+        System.out.print("*");
+      }
+      System.out.println();
     }
   }
   public static void main(String[] args) {
@@ -18,7 +47,13 @@ public class Q14 {
         System.out.print("몇 단 삼각형입니까?: ");
         n = sc.nextInt();
     } while (n <= 0);
-    traiangleLB(n);
+    triangleLB(n);
+    System.out.println();
+    triangleLU(n);
+    System.out.println();
+    triangleRU(n);
+    System.out.println();
+    triangleRB(n);
     sc.close();
   }
 }
